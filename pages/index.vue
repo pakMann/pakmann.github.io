5fc143eca70eff3f884f2b3447dce97039fc3ec5
Arman Adhitama <black.biron@gmail.com>
2025-02-07 14:51:12 +0700

    minor update on the styles

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,8 @@ import Works from '~/components/Works.vue';
         //- p(style="font-size: 24px") Hi, I'm <span class="hightlight">Arman Adhitama</span>, a full stack developer with extensive experience in designing and maintaining web applications. Skilled in HTML, CSS/Sass, JavaScript (jQuery, Nuxt), PHP (Wordpress, CodeIgniter, OpenCart), and Ruby (Ruby on Rails, Sinatra, Padrino, Middleman), I build dynamic, scalable solutions. Proficient in database management (SQL, MongoDB), and RESTful APIs, I ensure seamless integration and optimal user experiences. Experienced in authentication, Agile development, and cloud platforms (AWS, Heroku), I leverage Docker for efficiency. A problem-solver and team player, I communicate effectively to align technical and business goals.
 
     Works
+
+    #copy Built with <a href="https://nuxt.com" target="_blank">Nuxt</a> and hosted on <a href="https://github.com/pakMann/pakmann.github.io" target="_blank">GitHub</a>.
 #footer
   a(href="/cv.pdf") Download CV (PDF)
 </template>
