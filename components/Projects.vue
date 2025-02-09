1f3d934461a5c749a34a5dc7ba0788f22d7aac8a
Arman Adhitama <black.biron@gmail.com>
2025-02-10 01:06:35 +0700

    update

---
+++ components/Projects.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+section#works.row.justify-content-center
+  .col-md-3
+    h2.position-sticky Projects
+
+  .col-md-6
+    //- div.work.mb-5(v-for="work in works" :key="work.company")
+    //-   .header
+    //-     .left-header
+    //-       h3.role {{ work.role }}
+    //-       h3.company(v-html="marked(work.company)")
+
+    //-     .dots
+
+    //-     .right-header
+    //-       h3.period {{ work.period }}
+    //-       h3.location {{ work.location }}
+
+    //-   //- div(v-html="marked(work.about)")
+    //-   div.desc(v-html="marked(work.objectives)")
+</template>
